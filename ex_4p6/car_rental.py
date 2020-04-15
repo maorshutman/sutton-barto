9331d8652ee567f51e4b20c33bf9d5cd3005aef1
maors <maor.shutman@samsung.com>
2020-04-15 12:08:38 +0000

    Converging, but not to the solution in the book

--- ex_4p6/car_rental.py
+++ ex_4p6/car_rental.py
@@ -6,8 +6,10 @@ import numpy as np
 
 _GAMMA = 0.9
 _MAX_CARS = 20
-_THETA = 0.5
+_THETA = 0.1
 _A_MAX = 5
+_RENT_REWARD = 10.
+_MOVE_REWARD = -2.
 _ACTIONS = list(range(-_A_MAX, _A_MAX + 1))
 
 
@@ -16,19 +18,21 @@ def policy_iteration(V, policy):
     V_old = V.copy()
 
     while True:
-        V_new = eval_policy(V_old, policy)
+        V_new = eval_policy(V_old, policy_old)
         policy_new, policy_stable = improve_policy(policy_old, V_new)
 
-        print_policy(-policy_new)
+        # DEBUG
+        print_policy(policy_new)
+        print_values(V_new)
+
+        if policy_stable:
+            break
 
         # DEBUG
         np.save('p1.npy', policy_new)
 
         policy_old = policy_new.copy()
         V_old = V_new.copy()
-
-        if policy_stable:
-            break
 
     return V_new, policy_new
 
@@ -39,11 +43,12 @@ def eval_policy(V, policy):
 
     while True:
         delta = 0
-        for n1 in range(_MAX_CARS):
-            for n2 in range(_MAX_CARS):
+        for n1 in range(_MAX_CARS + 1):
+            for n2 in range(_MAX_CARS + 1):
                 a = policy[n1, n2]
-                V_new[n1, n2] = calc_mean_value(V_old, (n1, n2), a)
-                delta = max(delta, np.abs(V_new[n1, n2] - V_old[n1, n2]))
+                if legal_action((n1, n2), a):
+                    V_new[n1, n2] = calc_mean_value(V_old, (n1, n2), a)
+                    delta = max(delta, np.abs(V_new[n1, n2] - V_old[n1, n2]))
 
         V_old = V_new.copy()
 
@@ -56,41 +61,49 @@ def eval_policy(V, policy):
 
 
 def improve_policy(policy, V):
-    new_policy = policy.copy()
+    new_policy = np.zeros_like(policy)
 
     policy_stable = True
-    for n1 in range(_MAX_CARS):
-        for n2 in range(_MAX_CARS):
+    for n1 in range(_MAX_CARS + 1):
+        for n2 in range(_MAX_CARS + 1):
             old_action = policy[n1, n2]
+
             a = best_action((n1, n2), V)
             new_policy[n1, n2] = a
 
-            if a != old_action:
+            if int(a) != int(old_action):
                 policy_stable = False
 
     return new_policy, policy_stable
 
 
+def legal_action(s, a):
+    return (s[0] - a >= 0) and (s[1] + a >= 0) and \
+           (s[0] - a <= _MAX_CARS) and (s[1] + a <= _MAX_CARS)
+
+
+
 def best_action(s, V):
     """Return pi(s)."""
     max_val = -1e10
-    best_action = -10
+    best_action = -_A_MAX - 1
     for a in _ACTIONS:
-        v = calc_mean_value(V, s, a)
-        if v > max_val:
-            max_val = v
-            best_action = a
+        if legal_action(s, a):
+            v = calc_mean_value(V, s, a)
+            if v > max_val:
+                max_val = v
+                best_action = a
+
+    assert best_action != (-_A_MAX - 1)
     return best_action
 
 
 def calc_mean_value(V, s, a):
-    # Check if action is valid.
-    if not ((s[0] + a >= 0) and (s[1] - a >= 0)):
-        return 0.
+    assert legal_action(s, a)
 
     v = 0.
-    for n1 in range(_MAX_CARS):
-        for n2 in range(_MAX_CARS):
+    for n1 in range(_MAX_CARS + 1):
+        for n2 in range(_MAX_CARS + 1):
             prob, exp_reward = p_env((n1, n2), s, a)
             v += exp_reward + _GAMMA * V[n1, n2] * prob
 
@@ -110,6 +123,11 @@ def p_env(s_next, s, a):
     mu1_return = 3
     mu2_return = 2
 
+    # mu1_rental = 1e-10
+    # mu2_rental = 1e-10
+    # mu1_return = 1e-10
+    # mu2_return = 1e-10
+
     # Before action - end of prev day.
 
     n1, n2 = s
@@ -118,9 +136,9 @@ def p_env(s_next, s, a):
 
     # After the action. We account for the fact that if there are too many cars
     # they disappear.
-    assert ((n1 + a) >= 0) and ((n2 - a) >= 0)
-    n1 += min(a, _MAX_CARS)
-    n2 -= min(a, _MAX_CARS)
+    assert legal_action((n1, n2), a)
+    n1 = min(n1 - a, _MAX_CARS)
+    n2 = min(n2 + a, _MAX_CARS)
 
     # After action - next day begins.
 
@@ -128,31 +146,36 @@ def p_env(s_next, s, a):
     assert ((n1_next <= _MAX_CARS) and (n1_next >= 0))
     assert ((n2_next <= _MAX_CARS) and (n2_next >= 0))
 
-    # Init with reward due to moving cars.
-    exp_reward = -2 * np.abs(a)
+    # Justify this decomposition.
+    # TODO
 
+    # Location 1.
     p_n1_next = 0.
+    exp_reward_1 = 0.
     for rental in range(0, _MAX_CARS + 1):
         actually_rented = min(rental, n1)
         required_ret = n1_next - n1 + actually_rented
         if required_ret >= 0:
-            p_n1_next += poisson(rental, mu1_rental) *\
-                         poisson(required_ret, mu1_return)
-            exp_reward += p_n1_next * (10. * actually_rented)
+            p = poisson(rental, mu1_rental) * poisson(required_ret, mu1_return)
+            p_n1_next += p
+            exp_reward_1 += p * (_RENT_REWARD * actually_rented)
 
+    # Location 2.
     p_n2_next = 0.
+    exp_reward_2 = 0.
     for rental in range(0, _MAX_CARS + 1):
         actually_rented = min(rental, n2)
         required_ret = n2_next - n2 + actually_rented
         if required_ret >= 0:
-            p_n2_next += poisson(rental, mu2_rental) *\
-                         poisson(required_ret, mu2_return)
-            exp_reward += p_n2_next * (10. * actually_rented)
+            p = poisson(rental, mu2_rental) * poisson(required_ret, mu2_return)
+            p_n2_next += p
+            exp_reward_2 += p * (_RENT_REWARD * actually_rented)
 
-    assert (p_n1_next <= 1.) and (p_n2_next <= 1.)
+    exp_reward = _MOVE_REWARD * np.abs(a) * p_n1_next * p_n2_next + \
+                 p_n1_next * exp_reward_2 + p_n2_next * exp_reward_1
 
-    # As these are independent events we have a product.
     prob = p_n1_next * p_n2_next
+    assert prob <= 1.
 
     return prob, exp_reward
 
@@ -168,10 +191,16 @@ def print_policy(policy):
 
 
 def main():
-    V = np.zeros((_MAX_CARS, _MAX_CARS))
-    policy = np.zeros((_MAX_CARS, _MAX_CARS))
+    V = np.zeros((_MAX_CARS + 1, _MAX_CARS + 1))
+    policy = np.zeros((_MAX_CARS + 1, _MAX_CARS + 1))
 
     policy_iteration(V, policy)
+
+    # tot = 0.
+    # for i in range(_MAX_CARS + 1):
+    #     for j in range(_MAX_CARS + 1):
+    #         tot += p_env((i, j), (2, 5), 0)[0]
+    # print(tot)
 
 
 if __name__ == "__main__":
